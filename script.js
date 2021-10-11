3518615143e76ac1ab83b9fa4740eb0b6a211334
Ever <evermedina2025@gmail.com>
2021-10-10 19:08:34 -0700

    header

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ $(document).ready(function() {
     const currentDate = moment().format('dddd MMMM Do, YYYY'); 
      $("#currentDay").text(currentDate);
 
-    // Current Time in 12 hour format
-    const currentHour = moment().format('h:[00]')
-    console.log(currentHour)
+        // Current Time in 12 hour format
+        const currentHour = moment().format('h:[00]');
+        console.log(currentHour)
 
 
 var timeBlockEl = document.createElement("div")
 $(timeBlockEl).addClass("time-block")
 var timeEl= document.createElement("div")
- $(timeEl).addClass("input-group-text").text("9 a.m")
+ $(timeEl).addClass("input-group-text").text("9 a.m.")
  var inputEl = document.createElement("div")
 $(inputEl).addClass("form-control").text("Input")
 var btnEl = document.createElement("button")
-$(btnEl).addClass("saveBtn").text("save")
-$(timeEl).text("9 a.m")
+$(btnEl).addClass("saveBtn").text("Save")
+$(timeEl).text("9 a.m.")
 $(inputEl).text("Input")
 $(btnEl).text("Save")
 
-$( ".container" ).append(timeBlockEl);
+$(".container").append(timeBlockEl);
 $( timeBlockEl ).append(timeEl);
 $( timeBlockEl ).append(inputEl);
 $( timeBlockEl ).append(btnEl);
-console.log(timeBlockEl)
-$( ".container" ).append(timeBlockEl);
+
+
+
+
 
 })
